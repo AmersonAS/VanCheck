cc0c12d9a7b97a5c38d2cba1c8aa1198c42fb7cb
Nathan Augusto <nathanaugusto_designer@outlook.com>
2023-11-07 22:31:14 -0300

    Primeiro commit

---
+++ tab-bar/pages/Home/Home.js
@@ -0,0 +1,113 @@
+import { Image, Button, StyleSheet, Text, TextInput, View, TouchableOpacity, ImageBackground} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient'
+import { BlurView } from 'expo-blur';
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+          <LinearGradient style={styles.gradientBackground}
+            colors={['#293A80', '#010038']}
+            start={{ x: 0, y: 1.5 }}
+            end={{ x: 0, y: -0.1 }}/>
+
+          <View style={styles.header}>
+            <Image source={require('./assets/ExeLogo.png')} style={[styles.LogoImage, {width:30, height:30}]}/>
+
+            <Text style={styles.TextPassageiro}>Passageiro | Nathan Silva</Text>
+          </View>
+
+          <View style={styles.transparentBar}>
+            <Text style={styles.textViagens}>Viagens</Text>
+          </View>
+
+          <View style={styles.content}>
+            <ImageBackground source={require('./assets/Rota-Enfeite.png')} style={styles.backImage}>
+              <TouchableOpacity style={styles.embarcarButton}>
+                <Text style={styles.buttonText}>Embarcar</Text>
+              </TouchableOpacity>
+            </ImageBackground>
+          </View>
+        </View>
+        
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+      },
+
+      header:{
+        paddingTop:25,
+        paddingBottom:12,
+        alignItems: 'center',
+        backgroundColor: '#1A1B28',
+        width: '100%',
+      },
+
+      LogoImage:{
+        marginBottom:5,
+      },
+
+      TextPassageiro:{
+        fontSize:10,
+        color:'#eeeeee',
+      },
+
+      content:{
+        flex:1,
+      },
+    
+      gradientBackground: {
+        flex: 1,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+      },
+
+      transparentBar:{
+        padding:15,
+        alignItems: 'center',
+        width:'100%',
+        backgroundColor:'rgba(38, 39, 50, 0.3)',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 5,
+        },
+        shadowOpacity: 0.2, // Opacidade da sombra
+        shadowRadius: 5, // Raio da sombra
+        elevation: 5, // Sombra no Android (não é necessário no iOS)
+      },
+
+      textViagens:{
+        color: '#FFFFFF',
+        fontWeight:'bold',
+      },
+
+      backImage:{
+        justifyContent:'flex-end',
+        alignItems:'center',
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+      },
+
+      embarcarButton:{
+        backgroundColor:'#F39422',
+        width:'40%',
+        height:'9%',
+        borderRadius:15,
+        justifyContent:'center',
+        alignItems:'center',
+        marginBottom:'24%',
+      },
+
+      buttonText:{
+        color:'#FFFFFF',
+        fontWeight:'bold',
+        fontSize:16,
+      }
+})
---
+++ tab-bar/pages/Routes.js
@@ -0,0 +1,78 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+import Home from './Home/Home'
+import Options from './Options/Options'
+import Profile from './Profile/Profile'
+
+import {Ionicons} from '@expo/vector-icons'
+
+const Tab = createBottomTabNavigator();
+
+function Routes() {
+    return(
+        <Tab.Navigator 
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarStyle:{
+                position: 'absolute',
+                backgroundColor: '#1A1B28',
+                borderTopWidth: 0,
+
+                bottom:14,
+                left:14,
+                right:14,
+                elevation:0,
+                borderRadius:15,
+                height:65,
+
+            }
+        }}>
+            <Tab.Screen
+            name='Home'
+            component={Home}
+            options={{
+                headerShown: false,
+                tabBarIcon: ({color, size, focused}) =>{
+                    if(focused){
+                        return <Ionicons name="bus" size={size} color={'#F39422'}/>
+                    }
+
+                    return <Ionicons name="bus-outline" size={size} color={'#EEEEEE'}/>
+                }
+            }}
+            />
+        
+        <Tab.Screen
+            name='Profile'
+            component={Profile}
+            options={{
+                headerShown: false,
+                tabBarIcon: ({color, size, focused}) =>{
+                    if(focused){
+                        return <Ionicons name="person" size={size} color={'#F39422'}/>
+                    }
+
+                    return <Ionicons name="person-outline" size={size} color={'#EEEEEE'}/>
+                }
+            }}
+            />
+
+        <Tab.Screen
+            name='Options'
+            component={Options}
+            options={{
+                headerShown: false,
+                tabBarIcon: ({color, size, focused}) =>{
+                    if(focused){
+                        return <Ionicons name="settings" size={size} color={'#F39422'}/>
+                    }
+
+                    return <Ionicons name="settings-outline" size={size} color={'#EEEEEE'}/>
+                }
+            }}
+            />
+        </Tab.Navigator>
+    )
+}
+
+export default Routes;
