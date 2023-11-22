457c329d183128d17214949b32e7b629f8cbd171
Nathan Augusto <nathanaugusto_designer@outlook.com>
2023-11-22 13:23:34 -0300

    Estilização da screen 'Profile'

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,16 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ImageBackground, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { auth } from '../config/firebase';
 import { useNavigation } from '@react-navigation/native';
 import { signOut, deleteUser } from 'firebase/auth';
 
 const Profile = () => {
-  const [opcaoSelecionada, setOpcaoSelecionada] = useState(null);
   const navigation = useNavigation();
-
-  const handlePress = (opcao) => {
-    setOpcaoSelecionada(opcao);
-  };
 
   const handleLogout = async () => {
     try {
@@ -45,33 +41,124 @@ const Profile = () => {
   };
 
   return (
-    <View>
-      <Text>Opção Selecionada: {opcaoSelecionada}</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.backgroundImage}
+        source={require('../../assets/Steps/BackImage-Step5.png')}
+      >
+        <View>
+          <View style={styles.header}>
+            <Text>Perfil</Text>
+            <TouchableOpacity style={styles.editImage}>
+              <Image style={{width:100, height:100}} source={require('../../assets/ImageUser.png')}/>
+            </TouchableOpacity>
+          </View>
 
-      <TouchableOpacity style={styles.botao} onPress={handleLogout}>
-        <Text style={styles.texto}>Logout</Text>
-      </TouchableOpacity>
+          <View style={styles.content}>
+            <LinearGradient
+              style={styles.gradientBackground}
+              colors={['#293A80', '#010038']}
+              start={{ x: 0, y: 1.5 }}
+              end={{ x: 0, y: -0.1 }}
+            />
+{/*----------------------------------------------------------------------------------------------------------------------*/}
 
-      <TouchableOpacity style={styles.botao} onPress={handleDeleteCurrentUser}>
-        <Text style={styles.texto}>Excluir Conta</Text>
-      </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.textButton}>Senha</Text>
+            </TouchableOpacity>
+{/*----------------------------------------------------------------------------------------------------------------------*/}
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.textButton}>E-mail</Text>
+            </TouchableOpacity>
+{/*----------------------------------------------------------------------------------------------------------------------*/}
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.textButton}>Contato</Text>
+            </TouchableOpacity>
+{/*----------------------------------------------------------------------------------------------------------------------*/}
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.textButton}>Notificações</Text>
+            </TouchableOpacity>
+{/*----------------------------------------------------------------------------------------------------------------------*/}
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.textButton}>Contate-nos</Text>
+            </TouchableOpacity>
+{/*----------------------------------------------------------------------------------------------------------------------*/}
+
+            <TouchableOpacity style={styles.button} onPress={handleLogout}>
+              <Text style={styles.REDtextButton}>Sair da conta</Text>
+            </TouchableOpacity>
+{/*----------------------------------------------------------------------------------------------------------------------*/}
+
+            <TouchableOpacity style={styles.button} onPress={handleDeleteCurrentUser}>
+              <Text style={styles.REDtextButton}>Excluir Conta</Text>
+            </TouchableOpacity>
+{/*----------------------------------------------------------------------------------------------------------------------*/}
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  botao:{
-    alignSelf: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'blue',
-    width:100,
-    height:30,
-    margin: 30,
+  container: {
+    flex: 1,
+    backgroundColor:'#1A1B28',
   },
 
-  texto:{
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  //--------------------------------------------------------------
+  header:{
+    backgroundColor: 'rgba(60, 60, 158, 0.3)',
+  }, 
+
+  content:{
+    backgroundColor: 'rgba(60, 60, 158, 0.6)',
+    width: '100%',
+    alignItems: 'center',
+  },
+
+  gradientBackground: {
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+  //--------------------------------------------------------------
+
+  button: {
+    width: 380,
+    height: 60,
+    backgroundColor: 'rgba(60, 60, 158, 0.3)',
+    borderRadius: 14,
+    marginBottom: 20,
+
+    borderWidth: 1,
+    borderColor: 'rgba(255, 255, 255, 0.08)',
+
+    paddingHorizontal: 20,
+    alignItems: 'left',
+    justifyContent: 'center',
+  },
+
+  textButton:{
     color: 'white',
+    fontSize: 17,
+  },
+
+  REDtextButton:{
+    color: '#E31144',
+    fontSize: 17,
+    fontWeight: 'bold',
   }
 })
 
